d3b40dec70af0cd801a03efb1697814a1c1361d5
Ben Wander <ben@thewandering.band>
2022-06-24 15:05:44 -0600

    first commit

---
+++ Source/TaxiGame/TaxiGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TaxiGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAXIGAME_API ATaxiGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
